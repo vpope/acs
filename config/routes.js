9fe22c85b9dd699b2e19a6ea3d2e1dec3c2b8c56
Victoria Pope <vpope94@gmail.com>
2015-07-16 18:43:21 -0500

    added in more photos to image folder as well as added in carousel for thumbnails

--- config/routes.js
+++ config/routes.js
@@ -21,34 +21,38 @@
  */
 
 module.exports.routes = {
-    /***************************************************************************
-     *                                                                          *
-     * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-     * etc. depending on your default view engine) your home page.              *
-     *                                                                          *
-     * (Alternatively, remove this and add an `index.html` file in your         *
-     * `assets` directory)                                                      *
-     *                                                                          *
-     ***************************************************************************/
+/***************************************************************************
+ *                                                                          *
+ * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+ * etc. depending on your default view engine) your home page.              *
+ *                                                                          *
+ * (Alternatively, remove this and add an `index.html` file in your         *
+ * `assets` directory)                                                      *
+ *                                                                          *
+ ***************************************************************************/
 
-    '/': {
-        view: 'homepage'
-    },
-    '/graphic': {
+'/': {
+view: 'homepage'
+},
+        '/graphic': {
         view: 'graphic'
-    },
-    '/illustration': {
+        },
+        '/illustration': {
         view: 'illustration'
-    }
+        },
+        '/ball-carousel' : {
+        view: 'ball-carousel'
+        }
 
-    /***************************************************************************
-     *                                                                          *
-     * Custom routes here...                                                    *
-     *                                                                          *
-     * If a request to a URL doesn't match any of the custom routes above, it   *
-     * is matched against Sails route blueprints. See `config/blueprints.js`    *
-     * for configuration options and examples.                                  *
-     *                                                                          *
-     ***************************************************************************/
+
+/***************************************************************************
+ *                                                                          *
+ * Custom routes here...                                                    *
+ *                                                                          *
+ * If a request to a URL doesn't match any of the custom routes above, it   *
+ * is matched against Sails route blueprints. See `config/blueprints.js`    *
+ * for configuration options and examples.                                  *
+ *                                                                          *
+ ***************************************************************************/
 
 };
